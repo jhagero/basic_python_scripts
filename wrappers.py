0b74ca9e9f86f2c76c4268380a941304e4e9a97c
jhagero <65025730+jhagero@users.noreply.github.com>
2025-11-01 21:00:04 -0400

    enhanced wrappers

--- wrappers.py
+++ wrappers.py
@@ -1,25 +1,56 @@
-def my_decorator(func):
-    def wrapper():
-        print(f"Trying {func.__name__}")
+import time
+from datetime import datetime, timezone
+
+def smart_wrapper(func):
+    def wrapper(*args, **kwargs):
         try:
-            func()
-            print("Complete")
+            start = time.time()
+            # -> Convert to readable timestamp
+            timestamp = datetime.fromtimestamp(start, tz=timezone.utc)
+            print(f"Trying {func.__name__}() @[{timestamp.strftime('%H:%M:%S.%f')[:-3]}s]")
+
+            result = func(*args, **kwargs) # <- Any args
+            elapsed = (time.time() - start)*1000
+
+            print(f"Completed {func.__name__}() in {elapsed:.3f}ms")
+            return result
         except Exception as e:
             print(f"{func.__name__} failed! {e}")
     return wrapper
 
-@my_decorator
+@smart_wrapper
 def do_this():
     print("Doing this!")
 
-@my_decorator
+@smart_wrapper
 def do_that():
     print("Doing that!")
 
-@my_decorator
+@smart_wrapper
 def risky():
     x = 1 / 0
+
+@smart_wrapper
+def slow_sleep(n):
+    time.sleep(n)
+
+@smart_wrapper
+def fast_math(x, y):
+    return x ** y
+
+@smart_wrapper
+def load_level():
+    # heavy YAML + GLTF
+    ...
+    
+@smart_wrapper
+def update_physics():
+    # collision checks
+    ...
+
 
 do_that()
 do_this()
 risky()
+slow_sleep(1)       # -> slow_sleep(): ...
+fast_math(2, 10)    # -> fast_math(): ...
